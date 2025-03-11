8d53f71cbacc07d099d7e51a33cf66ab01b6be83
Thomas Gfüllner <thomas.gfuellner@gmail.com>
2025-03-11 14:29:55 +0100

    feat> python

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -215,7 +215,7 @@ return {
         yamlls = {},
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
