7e8e7172d2a74c95d76a99282e7660ae61382058
Thomas Gfüllner <thomas.gfuellner@gmail.com>
2025-03-20 20:03:02 +0100

    indenline mit dezenteren Farben

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -8,26 +8,16 @@ return {
     config = function()
       local ibl = require 'ibl'
       local highlight = {
-        'RainbowRed',
-        'RainbowYellow',
-        'RainbowBlue',
-        'RainbowOrange',
-        'RainbowGreen',
-        'RainbowViolet',
-        'RainbowCyan',
+        'iblColor1',
+        'iblColor2',
       }
 
       local hooks = require 'ibl.hooks'
       -- create the highlight groups in the highlight setup hook, so they are reset
       -- every time the colorscheme changes
       hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-        vim.api.nvim_set_hl(0, 'RainbowRed', { fg = '#E06C75' })
-        vim.api.nvim_set_hl(0, 'RainbowYellow', { fg = '#E5C07B' })
-        vim.api.nvim_set_hl(0, 'RainbowBlue', { fg = '#61AFEF' })
-        vim.api.nvim_set_hl(0, 'RainbowOrange', { fg = '#D19A66' })
-        vim.api.nvim_set_hl(0, 'RainbowGreen', { fg = '#98C379' })
-        vim.api.nvim_set_hl(0, 'RainbowViolet', { fg = '#C678DD' })
-        vim.api.nvim_set_hl(0, 'RainbowCyan', { fg = '#56B6C2' })
+        vim.api.nvim_set_hl(0, 'iblColor1', { fg = '#424242' })
+        vim.api.nvim_set_hl(0, 'iblColor2', { fg = '#6F749F' })
       end)
 
       ibl.setup {
