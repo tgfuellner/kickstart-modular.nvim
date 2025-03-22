5badf0eae12d7eb7fcfebaaeff0706ec4eddf7d4
Thomas Gfüllner <thomas.gfuellner@gmail.com>
2025-03-22 17:33:53 +0100

    Anzahl folded Zeilen

--- lua/custom/plugins/folding.lua
+++ lua/custom/plugins/folding.lua
@@ -13,6 +13,7 @@ return {
     -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
     vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
     vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+    vim.keymap.set('n', '=', 'za', { desc = 'Toggle fold (za)' })
 
     -- Tell the server the capability of foldingRange,
     -- Neovim hasn't added foldingRange to default capabilities, users must add it manually
@@ -28,6 +29,37 @@ return {
         -- you can add other fields for setting up lsp server in this table
       }
     end
-    require('ufo').setup()
+
+    local handler = function(virtText, lnum, endLnum, width, truncate)
+      local newVirtText = {}
+      local suffix = (' 󰁂 %d folded'):format(endLnum - lnum)
+      local sufWidth = vim.fn.strdisplaywidth(suffix)
+      local targetWidth = width - sufWidth
+      local curWidth = 0
+      for _, chunk in ipairs(virtText) do
+        local chunkText = chunk[1]
+        local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+        if targetWidth > curWidth + chunkWidth then
+          table.insert(newVirtText, chunk)
+        else
+          chunkText = truncate(chunkText, targetWidth - curWidth)
+          local hlGroup = chunk[2]
+          table.insert(newVirtText, { chunkText, hlGroup })
+          chunkWidth = vim.fn.strdisplaywidth(chunkText)
+          -- str width returned from truncate() may less than 2nd argument, need padding
+          if curWidth + chunkWidth < targetWidth then
+            suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+          end
+          break
+        end
+        curWidth = curWidth + chunkWidth
+      end
+      table.insert(newVirtText, { suffix, 'MoreMsg' })
+      return newVirtText
+    end
+
+    require('ufo').setup {
+      fold_virt_text_handler = handler,
+    }
   end,
 }
