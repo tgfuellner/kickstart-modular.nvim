fdd74d55655e40da486ae258af2cb939cccb039c
Thomas Gfüllner <thomas.gfuellner@gmail.com>
2025-05-20 13:38:30 +0200

    feat: allow config in Files

--- lua/options.lua
+++ lua/options.lua
@@ -3,6 +3,9 @@
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
+-- Allow Vim config per File
+vim.opt.modeline = true
+
 -- Make line numbers default
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
